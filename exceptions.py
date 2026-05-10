8dd0a4df7541a44264bb35bafa5b317f79f6eb24
TatianaMZ3 <149605303+TatianaMZ3@users.noreply.github.com>
2026-05-09 20:06:20 -0500

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -15,3 +15,22 @@ class BaseExceptionHandler(ABC):
     ):
         """This method should be implemented for handling errors"""
         pass
+        class ClienteError(Exception):
+    """Excepción base para errores de cliente"""
+    pass
+
+
+class NombreInvalidoError(ClienteError):
+    pass
+
+
+class EdadInvalidaError(ClienteError):
+    pass
+
+
+class CorreoInvalidoError(ClienteError):
+    pass
+
+
+class TelefonoInvalidoError(ClienteError):
+    pass
